f7e62e3ddc2fe8763d4ccee900a710f82d4d7aad
co012 <co123@interia.eu>
2020-12-28 17:18:10 +0100

    calculateDiscountWithId4 maybe complete

--- functions_and_procedures/calculateDiscountWithId4.sql
+++ functions_and_procedures/calculateDiscountWithId4.sql
@@ -8,14 +8,21 @@ DECLARE @minDiscount REAL;
 DECLARE @maxDiscount REAL;
 DECLARE @minPrice MONEY;
 DECLARE @minOrders INT;
+DECLARE @nowYM INT;
 
 SET @minDiscount = (SELECT CurrentMinDiscount FROM DiscountsTypes WHERE DiscountTypeId = @typeId);
 SET @maxDiscount = (SELECT CurrentMaxDiscount FROM DiscountsTypes WHERE DiscountTypeId = @typeId);
 SET @minPrice = (SELECT MinPrice FROM DiscountsTypes WHERE DiscountTypeId = @typeId);
 SET @minOrders = (SELECT MinOrders FROM DiscountsTypes WHERE DiscountTypeId = @typeId);
-SET @from = (SELECT YM FROM
-(SELECT dbo.GETYEARMOUNTH(OrderMadeDate) as YM,FinalPrice 
-FROM Orders WHERE dbo.GETYEARMOUNTH(OrderMadeDate)  < dbo.GETYEARMOUNTH(GETDATE()) AND CustomerId = @customerId) as T)
+SET @nowYM = dbo.getYearMonth(GETDATE());
+SET @from = ( SELECT TOP 1 YM FROM
+(SELECT YM,Count(*) as OrdersMade, sum(FinalPrice) as TotalPrice FROM
+(SELECT dbo.getYearMonth(OrderMadeDate) as YM,FinalPrice 
+FROM Orders WHERE dbo.getYearMonth(OrderMadeDate)  < @nomYM AND CustomerId = @customerId AND NOT StatusId = 5 ) as T GROUP BY T.YM ORDER BY T.YM DESC) AS T2
+WHERE T2.OrdersMade < @minOrders OR T2.TotalPrice < @minPrice
+);
 
-RETURN 0;
+IF @from = NULL SET @from = 0;
+
+RETURN (SELECT COUNT(DISTINCT dbo.getYearMonth(OrderMadeDate)) FROM Orders WHERE dbo.getYearMonh(OrderMadeDate) > @from);
 END
